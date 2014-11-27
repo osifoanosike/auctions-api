c000e9f6b6b3896d93708e07d67dec5beca36b22
Anosike Osifo <osifo@dealdey.com>
2014-11-27 18:03:56 +0100

    add create action

--- app/controllers/api/v1/auctions_controller.rb
+++ app/controllers/api/v1/auctions_controller.rb
@@ -12,7 +12,7 @@ module Api
 				# end			
 			end
 
-
+			#this handles the api post request
 			def create
 			    @current_user = User.find_by(id: 5)
 			  	@auctionItem = @current_user.auction_items.build(auction_params)
@@ -20,14 +20,18 @@ module Api
 			    @auctionItem.absolute_url =  "#{request.protocol}#{request.host_with_port}#{ @auctionItem.image}"
 			    
 			  	if @auctionItem.save
-			  		flash[:success] = "Auction item was successfully added, #{@auctionItem.absolute_url}"
-			  		redirect_to auctions_path
+			  		# flash[:success] = "Auction item was successfully added, #{@auctionItem.absolute_url}"
+			  		render json: @auctionItem, status: :created
 			  	else
-			  		render 'new'
+			  		render json: @auctionsItem.errors, status: :unprocessable_entity
 			  	end
 			end
 
+			private
 
+				def auction_params
+					params.require(:auction_item).permit(:code, :name, :decription, :status, :quantity) if params[:auction_item]
+				end
 		end
 	end
 end
